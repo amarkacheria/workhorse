d56ce875f637daac45bdd4925f38053cef4b5c76
Amar Kacheria <ark81@case.edu>
2020-03-05 18:51:34 -0500

    initializing repo with boilerplate code

---
+++ ares_test/lib/client/client.js
@@ -0,0 +1,26 @@
+﻿/*
+#========================================================================================================================================== #
+................................................................ARES TEST......................................................................
+#========================================================================================================================================== #
+*/
+
+
+//Send the drone gps coordinates to a websocket server located at ws://localhost:8081.
+//Console log server response to message.
+
+
+const drone = {
+	drone_test_version: "1",
+	ws: require('ws'),
+    ws_socket: require('./ws_socket.js'),
+	drone_gps: {
+		latitude: 39.3151819149,
+		longitude: -84.16808592,
+		altitude_meters: 30
+	},
+	ws_client: {
+		ws_url: 'ws://localhost:8081',
+		connected: false,
+        connection: null
+	}
+};
---
+++ ares_test/lib/client/ws_socket.js
@@ -0,0 +1,9 @@
+﻿var ws_socket = function(drone)
+	{
+
+			//New WS connection............................
+			drone.ws_client.connection = new drone.ws(drone.ws_client.ws_url);
+	};
+
+
+module.exports = ws_socket;
